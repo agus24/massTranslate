035a491ee6e6a7c5c5e9b5b05fc73b14d571e6b0
agus24 <agusx244@gmail.com>
2018-04-26 07:17:36 +0700

    upd

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+require "vendor/autoload.php";
+ini_set('max_execution_time', -1);
+
+$kata = json_decode(file_get_contents("kata.json"),true);
+
+use Stichoza\GoogleTranslate\TranslateClient;
+
+$tr = new TranslateClient('id', 'su');
+
+$myfile = fopen("output.json", "rw");
+$output = [];
+foreach($kata as $kt) {
+    $output[]['kata'] = $tr->translate($kt['kata']);
+    $output[]['id'] = $kt['id'];
+}
+
+die(json_encode($output));
+fwrite($myfile, json_encode($output));
+fclose($myfile);
+
+function dd($var){
+    die(var_dump($var));
+}
